472bd138e0a74160c9d81c6e6d99b755c1038168
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-11-14 22:43:03 -0600

    Menu Hamburguesa
    Implementacion del menu hamburguesa al archivo del script global para que se pueda usar en todas las ventanas

--- js/script.js
+++ js/script.js
@@ -27,4 +27,32 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    let toggle = document.querySelector('.toggle');
+    let left = document.querySelector('.left');
+    let right = document.querySelector('.right');
+    let cerrar = document.querySelector('.close');
+    let body = document.querySelector('body');
 
+
+    toggle.addEventListener('click', () => {
+        toggle.classList.toggle('active');
+        left.classList.toggle('active');
+        right.classList.toggle('overlay');
+        body.style.overflow = 'hidden';
+    });
+    cerrar.onclick = () => {
+        toggle.classList.remove('active');
+        left.classList.remove('active');
+        right.classList.remove('overlay');
+        body.style.overflow = '';
+    };
+    window.onclick = (e) => {
+        if (e.target == right) {
+            toggle.classList.remove('active');
+            left.classList.remove('active');
+            right.classList.remove('overlay');
+            body.style.overflow = '';
+        }
+    };
+});
