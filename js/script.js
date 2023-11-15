e75ee6abb1f5567572aa71cb485ad38748c60909
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-11-15 12:22:17 -0600

    Calendario
    Funcionamiento del calendario con fechas reales

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.querySelector(".tasksDots");
     const ulList = document.querySelector(".tasks ul");
 
-    addButton.addEventListener("click", function() {
+    addButton.addEventListener("click", function () {
         const newLi = document.createElement("li");
 
         newLi.innerHTML = `
@@ -27,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+//MENU DE HAMBURGUESA
 document.addEventListener('DOMContentLoaded', function () {
     let toggle = document.querySelector('.toggle');
     let left = document.querySelector('.left');
@@ -55,4 +57,94 @@ document.addEventListener('DOMContentLoaded', function () {
             body.style.overflow = '';
         }
     };
+});
+
+
+
+//CALENDARIO
+$(document).ready(function () {
+    var monthName = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
+        "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    var now = new Date();
+    var day = now.getDate();
+    var month = now.getMonth();
+    var currentMonth = month;
+    var year = now.getFullYear();
+
+
+    initCalender();
+    console.log(startDay());
+
+    function initCalender() {
+        console.log(monthName[month]);
+        $("#text_month").text(monthName[month]);
+
+        $(".item_day").remove();
+        
+        for (let i = startDay(); i > 0; i--) {
+            $(".container .right main .container_calendar .calendarData .days").append
+                (`<span class="week_days_item item_day prev_days">${getTotalDays(month - 1) - (i - 1)}</span>`);
+        }
+
+        for (let i = 1; i <= getTotalDays(month); i++) {
+            if (i == day && month == currentMonth) {
+                $(".container .right main .container_calendar .calendarData .days").append
+                    (`<span class="week_days_item item_day today">${i}</span>`);
+            } else {
+                $(".container .right main .container_calendar .calendarData .days").append
+                    (`<span class="week_days_item item_day">${i}</span>`);
+            }
+        }
+    }
+    function getNextMonth() {
+        if (month !== 11) {
+            month++;
+        } else {
+            year++;
+            month = 0;
+        }
+        initCalender();
+    }
+    function getPrevMonth() {
+        if (month !== 0) {
+            month--;
+        } else {
+            year--;
+            month = 11;
+        }
+        initCalender();
+    }
+    function startDay() {
+        var start = new Date(year, month, 1);
+        return ((start.getDate() - 1) === -1) ? 6 : start.getDay();
+    }
+
+    function leapMonth() {
+        return ((year % 400 === 0) || (year % 4 === 0) && (year % 100 !== 0));
+    }
+
+    function getTotalDays() {
+        if (month === -1) month = 11;
+
+        var numMonthReal = month + 1;
+
+        if (numMonthReal == 1 || numMonthReal == 3 || numMonthReal == 5 || numMonthReal==7|| numMonthReal == 8 ||numMonthReal==10|| numMonthReal == 12) {
+            return 31;
+        } else if (numMonthReal == 4 || numMonthReal == 6
+            || numMonthReal == 9 || numMonthReal == 11) {
+            return 30;
+        } else {
+            return leapMonth() ? 29 : 28;
+        }
+    }
+
+    $("#next_month").click(function () {
+        getNextMonth();
+
+    });
+    $("#last_month").click(function () {
+        getPrevMonth();
+
+    })
 });
