9adc4422cd16e7ea5981f1945b08426a26167db7
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-11-16 11:32:55 -0600

    Avances
    avances

--- js/script.js
+++ js/script.js
@@ -172,9 +172,39 @@ document.addEventListener('DOMContentLoaded', function () {
 //Crear project cards
 document.addEventListener('DOMContentLoaded', function () {
 
-    createProjectCard();
-
-    function createProjectCard() {
+    createProjectCard("Tarea 1", "Casa", "50%", "Alta", "Lavar los trastes", "10/20/2023");
+    
+    function createProjectCard(tituloTarea, tag, progreso, prioridad, descripcionTarea, fechaEntrega, tituloTarea2, tag2, progreso2, prioridad2, descripcionTarea2, fechaEntrega2) {
+        if (tituloTarea == undefined) {
+            tituloTarea = "";
+        }
+        if (tituloTarea2 == undefined) {
+            tituloTarea2 = "";
+        }
+        if (tag == undefined) {
+            tag = "";
+        }
+        if (tag2 == undefined) {
+            tag2 = "";
+        }
+        if (prioridad == undefined) {
+            prioridad = "";
+        }
+        if (prioridad2 == undefined) {
+            prioridad2 = "";
+        }
+        if (descripcionTarea == undefined) {
+            descripcionTarea = "";
+        }
+        if (descripcionTarea2 == undefined) {
+            descripcionTarea2 = "";
+        }
+        if (fechaEntrega == undefined) {
+            fechaEntrega = "Sin limite de entrega";
+        }
+        if (fechaEntrega2 == undefined) {
+            fechaEntrega2 = "Sin limite de entrega";
+        }
         const containerProjectCards = document.querySelector("main");
         if (containerProjectCards) {
             const newProjectCard = document.createElement("div");
@@ -192,19 +222,15 @@ document.addEventListener('DOMContentLoaded', function () {
                     </div>
                     
                     <div class="projectProgress">
-                        <div class="process">
-                            <h2>${progreso}</h2>
-                        </div>
                         <div class="priority">
                             <h2>${prioridad}</h2>
                         </div>
                     </div>
                     
-
+    
                     
                     <div class="task">
                         <h2>${descripcionTarea}</h2>
-                        <span class="line"></span>
                     </div>
                     
                     <div class="due">
@@ -215,9 +241,45 @@ document.addEventListener('DOMContentLoaded', function () {
                 
             `;
             containerProjectCards.appendChild(newProjectCard);
+    
+            const newProjectCard2 = document.createElement("div");
+            newProjectCard2.className = "projectCard projectCard2";
+            newProjectCard2.innerHTML = `
+            <div class="projectCard">
+                    
+                    <div class="projectTop">
+                        <h2>${tituloTarea2}<br><span>${tag2}</span></h2>
+                        <div class="projectDots">
+                            <span class="material-symbols-outlined">
+                                more_horiz
+                            </span>
+                        </div>
+                    </div>
+                    
+                    <div class="projectProgress">
+                        <div class="priority">
+                            <h2>${prioridad2}</h2>
+                        </div>
+                    </div>
+                    
+    
+                    
+                    <div class="task">
+                        <h2>${descripcionTarea2}</h2>
+                    </div>
+                    
+                    <div class="due">
+                        <h2>${fechaEntrega2}</h2>
+                    </div>
+                    
+                </div>
+                
+            `;
+            containerProjectCards.appendChild(newProjectCard2);
+    
         } else {
             console.log("Error al agregar un projectCard");
         }
-
+    
     }
-});
+    });
