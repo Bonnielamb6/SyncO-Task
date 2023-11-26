d94943e33711d4380f4f32ef0d267e4378619d3d
Bonnielamb6 <edgartrabajos2205@gmail.com>
2023-11-25 22:27:36 -0600

    Bug fix post-correo personalizado

--- js/script.js
+++ js/script.js
@@ -1,33 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const addButton = document.querySelector(".tasksDots");
-//     const ulList = document.querySelector(".tasks ul");
-
-//     addButton.addEventListener("click", function () {
-//         const newLi = document.createElement("li");
-
-//         newLi.innerHTML = `
-//             <span class="tasksIconName">
-//                 <span class="tasksIcon done">
-//                     <span class="material-symbols-outlined">
-//                         check
-//                     </span>
-//                 </span>
-//                 <span class="tasksName">
-//                     Nueva Tarea
-//                 </span>
-//             </span>
-//             <span class="tasksStar full">
-//                 <span class="material-symbols-outlined">
-//                     star
-//                 </span>
-//             </span>
-//         `;
-
-//         ulList.appendChild(newLi);
-//     });
-// });
-
-
 //MENU DE HAMBURGUESA
 document.addEventListener('DOMContentLoaded', function () {
     let toggle = document.querySelector('.toggle');
@@ -58,30 +28,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 });
-
-
-
-
-
-
-
-//Mandar correo a persona de nueva tarea
-document.addEventListener('DOMContentLoaded', function () {
-
-
-    //sendEmail("edgartrabajos2205@gmail.com","Esta es una prueba con parametros");
-
-    function sendEmail(toEmail, message) {
-
-        // Envía el correo electrónico
-        emailjs.send("service_u4l7y3t", "template_7axdjtj", { to_email: toEmail, message })
-            .then(function (response) {
-                console.log("Correo enviado con éxito", response);
-            })
-            .catch(function (error) {
-                console.error("Error al enviar el correo", error);
-            });
-    }
-});
-
-
